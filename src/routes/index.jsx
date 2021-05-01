f5ccdc2b58af54e01eed925fd309aadede9f1d71
Tushar <iamtushar324@gmail.com>
2021-05-01 22:37:16 +0530

    Fixing Router Bug
    Signed-off-by: Tushar <iamtushar324@gmail.com>

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	Redirect,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import styled from "styled-components";
 
 import Homepage from "pages/Homepage";
@@ -15,36 +10,37 @@ import Events from "containers/Events";
 import EventDetails from "containers/Events/EventDetials";
 
 export default function App() {
-	return (
-		<MainWrapper>
-			{" "}
-			<Router>
-				<Header />
-				<Switch>
-					<Redirect exact from="/" to="/home" />
-					<Route exact path="/home">
-						<Homepage />
-					</Route>
-					<Route path="/events/:eventId">
-						<EventDetails />
-					</Route>
-					<Route path="/events">
-						<Events />
-					</Route>
-					<Route path="/blogs">
-						<Team />
-					</Route>
-					<Route path="/faq">
-						<Team />
-					</Route>
-					<Route path="/team">
-						<Team />
-					</Route>
-				</Switch>
-				<Footer />
-			</Router>
-		</MainWrapper>
-	);
+  return (
+    <MainWrapper>
+      {" "}
+      <Router>
+        <Header />
+        <Switch>
+          <Redirect exact from="/" to="/home" />
+          <Route exact path="/home">
+            <Homepage />
+          </Route>
+          <Route path="/events/:eventId">
+            <EventDetails />
+          </Route>
+          <Route path="/events">
+            <Events />
+          </Route>
+          <Route path="/blogs">
+            <Team />
+          </Route>
+          <Route path="/faq">
+            <Team />
+          </Route>
+          <Route path="/team">
+            <Team />
+          </Route>
+          <Route>404</Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </MainWrapper>
+  );
 }
 
 const MainWrapper = styled.div``;
