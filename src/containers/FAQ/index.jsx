9bb5b33401cac8bd9ec018367e71bb2507bad45a
harshphogat <55057156+harshphogat@users.noreply.github.com>
2021-05-07 22:44:06 +0530

    Update index.jsx

--- src/containers/FAQ/index.jsx
+++ src/containers/FAQ/index.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 
 const FAQ = () => {
-  return <div>FAQ</div>;
+  return <Container>
+    <Image src="https://static.wixstatic.com/media/bcb517_3c2b91ce63d942bc86ff5250060eb36d~mv2.gif" alt=" " />
+  </Container>;
 };
 
 export default FAQ;
+
+const Container = styled.div`
+	padding-top: 70px;
+	text-align: center;
+`
+const Image = styled.img`
+	margin-top: 30px;
+  `
