2b500651c9f5e3c539075c04bdc4b668afc3a159
harshphogat <55057156+harshphogat@users.noreply.github.com>
2021-05-07 19:49:25 +0530

    Update index.jsx

--- src/containers/Team/index.jsx
+++ src/containers/Team/index.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import styled from "styled-components";	
 import {getDeviceType} from "helpers";
+import TeamDetails from "./TeamDetails";
+import { Grid } from "@material-ui/core";
 
 const Team = () => {
 	return (
 		<Container>
-			<Heading>
-				Our Team
-			</Heading>
+			<Box>
+				<Heading>
+					Meet the Team
+				</Heading>
+			</Box>
 			<Image
 				src="https://i.pinimg.com/originals/70/9d/00/709d007dc00655a582927bdb1e08e080.jpg"
 			/>
+			<Grid container spacing={0}>
+			<Grid item xs={12} spacing ={3}>
+				<TeamDetails position ="President" name ="Suman Sangwan" />	
+				<TeamDetails position ="Member" name="Harsh"/>
+			</Grid>
+			</Grid>
 		</Container>
 	);
 };
@@ -18,23 +28,31 @@ const Team = () => {
 export default Team;
 
 const Container = styled.div`
-	width: ${getDeviceType() === "mobile" ? "75vw" : "100vw"};
-	padding-top: 70px;
-	text-align: center;
+	width: ${getDeviceType() === "mobile" ? "75vw" : "100%"};
+	padding-left:390px;
+	padding-top:80px;
+	margin:0 auto;
+	background-color:#f5f5f5;
+`;
+
+const Box = styled.div`
+	margin-top: 30px;
+	width: ${getDeviceType() === "mobile" ? "75vw" : "50vw"};
+	height: ${getDeviceType() === "mobile" ? "30vh" : "45vh"};
+	position:absolute;
+	display:flex;
+	justify-content:center;
+	background-color:rgba(0,0,0,0.2);
 `;
 
 const Heading = styled.h1`
-	padding: 3%;
 	font-size: ${getDeviceType() === "mobile" ? "30px" : "60px"};
-	align-items: center;
 	font-family: time;
-	color:#0e95d4;
-	display: flex;
-	position:absolute;
-	justify-content: center;
+	color:purple;
+
 `;
 const Image = styled.img`
 	margin-top: 30px;
-	height: 45vh;
-	width: 55%;
+	height: ${getDeviceType() === "mobile" ? "30vh" : "45vh"};
+	width: ${getDeviceType() === "mobile" ? "75vw" : "50vw"};
 `
