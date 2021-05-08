642921750a19270c3411f02bcd138358e06ba7da
harshphogat <55057156+harshphogat@users.noreply.github.com>
2021-05-08 18:03:08 +0530

    Update index.jsx

--- src/containers/Team/index.jsx
+++ src/containers/Team/index.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-import styled from "styled-components";
+import styled from "styled-components";	
+import {getDeviceType} from "helpers";
+
+import TeamBox from "./TeamBox";
 
 const Team = () => {
 	return (
 		<Container>
+			<Box>
+				<Heading>
+					Meet the Team
+				</Heading>
+			</Box>
 			<Image
-				src="https://static.wixstatic.com/media/bcb517_3c2b91ce63d942bc86ff5250060eb36d~mv2.gif"
-				alt=""
+				src="https://i.pinimg.com/originals/70/9d/00/709d007dc00655a582927bdb1e08e080.jpg"
 			/>
+			<TeamBox />
 		</Container>
 	);
 };
@@ -15,6 +23,7 @@ const Team = () => {
 export default Team;
 
 const Container = styled.div`
+<<<<<<< HEAD
 	padding-top: 70px;
 	min-height: 70vh;
 
@@ -25,6 +34,23 @@ const Container = styled.div`
 
 const Heading = styled.h1`
 	width: ${getDeviceType() === "mobile" ? "35vw" : "50vw"};
+=======
+	width: ${getDeviceType() === "mobile" ? "75vw" : "100%"};
+	padding-left:50px;
+	padding-top:80px;
+	margin:0 auto;
+	background-color:#f5f5f5;
+`;
+
+const Box = styled.div`
+	padding-left:340px;
+	margin-top: 30px;
+	position:absolute;
+`;
+
+const Heading = styled.h1`
+	width: ${getDeviceType() === "mobile" ? "75vw" : "50vw"};
+>>>>>>> team
 	height: ${getDeviceType() === "mobile" ? "30vh" : "45vh"};
 	font-size: ${getDeviceType() === "mobile" ? "30px" : "60px"};
 	font-family: time;
@@ -32,7 +58,17 @@ const Heading = styled.h1`
 	background-color:rgba(0,0,0,0.2);
 	display:flex;
 	justify-content:center;
+<<<<<<< HEAD
 `;
 const Image = styled.img`
 	max-width: 100vw;
+=======
+>>>>>>> team
 `;
+const Image = styled.img`
+	padding-left:340px;
+	margin-top: 30px;
+	background-size:cover;
+	height: ${getDeviceType() === "mobile" ? "30vh" : "45vh"};
+	width: ${getDeviceType() === "mobile" ? "75vw" : "50vw"};
+`
