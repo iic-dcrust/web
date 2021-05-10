fb066771146b5ed58c75837c663b450b42345494
Tushar <iamtushar324@gmail.com>
2021-05-10 21:44:21 +0530

    Fixed Faq's page UI
    Signed-off-by: Tushar <iamtushar324@gmail.com>

--- src/containers/FAQ/FAQCard.jsx
+++ src/containers/FAQ/FAQCard.jsx
@@ -1,51 +1,51 @@
 import React from "react";
 import styled from "styled-components";
-import {getDeviceType} from "helpers";
+import { getDeviceType } from "helpers";
 
-function FAQCard (content) {
-    return(        
-        <Container>
-            <Box>
-                <Text>{'\u2B26'}
-                    {content.ques}
-                </Text>
-                <Result>{'\u2b25'}
-                    {content.ans}
-                </Result>
-            </Box>
-        </Container>
-    );
+function FAQCard(content) {
+	return (
+		<Container>
+			<Box>
+				<Text>
+					{"\u2B26"}
+					{content.ques}
+				</Text>
+				<Result>
+					{"\u2b25"}
+					{content.ans}
+				</Result>
+			</Box>
+		</Container>
+	);
 }
 
 export default FAQCard;
 
 const Container = styled.div`
-padding-top:15px;
-padding-bottom:20px;
-margin:0 auto;
-justify-content:center;
-align-item:center;
-width:${getDeviceType() === "mobile" ? "50vw" :"75vw"};
-`
+	padding-top: 15px;
+	padding-bottom: 20px;
+	margin: 0 auto;
+	justify-content: center;
+	align-items: center;
+`;
 
-const Box = styled.div`
-width:${getDeviceType() === "mobile" ? "50vw" :"75vw"};
-`
+const Box = styled.div``;
 
 const Text = styled.h3`
-margin-top:10px;
-font-family:time;
-font-size:${getDeviceType() === "mobile" ? "15px" : "30px"};
-color:#3d84b8;
-border-radius:10px;
-`
+	margin-top: 10px;
+	font-family: time;
+	font-size: ${getDeviceType() === "mobile" ? "20px" : "30px"};
+	color: #3d84b8;
+	border-radius: 10px;
+`;
 
 const Result = styled.p`
-margin-top:20px;
-font-family:time;
-font-size:${getDeviceType() === "mobile" ? "7px" : "20px"};
-border-bottom:outset;
-border-top:inset;
-border-radius:10px;
-color:#435a64;
-`
+	margin-top: 20px;
+	font-family: time;
+	font-size: ${getDeviceType() === "mobile" ? "15px" : "20px"};
+	border-bottom: outset;
+	border-top: inset;
+	border-radius: 10px;
+	color: #435a64;
+	padding: 20px 10px;
+`;
--- src/containers/FAQ/index.jsx
+++ src/containers/FAQ/index.jsx
@@ -1,48 +1,51 @@
 import React from "react";
 import styled from "styled-components";
-import {getDeviceType} from "helpers";
+import { getDeviceType } from "helpers";
 import FAQDetails from "./FAQDetails";
 
 const FAQ = () => {
-  return (
-  <Container>
-    <Cover>
-      <Heading>FAQ</Heading>
-      </Cover>
-    <Image src="https://media.osram.info/media/img/osram-dam-300240/s,x,1260,y,0/338965_Faq_getty_82041747_cropped_672x260.jpg" alt=" " />
-    <FAQDetails />
-  </Container>
-  );
+	return (
+		<Container>
+			<Cover>
+				<Heading>FAQ</Heading>
+				<Image
+					src="https://media.osram.info/media/img/osram-dam-300240/s,x,1260,y,0/338965_Faq_getty_82041747_cropped_672x260.jpg"
+					alt=" "
+				/>
+			</Cover>
+			<FAQDetails />
+		</Container>
+	);
 };
 
 export default FAQ;
 
-
 const Container = styled.div`
 	padding-top: 70px;
-  margin:0 auto;
 	text-align: center;
-`
-const Cover =styled.div`
-  padding-left:285px;
-`
+`;
+const Cover = styled.div`
+	padding: ${getDeviceType() === "mobile" ? "30px 10vw" : "30px 10vw"};
+	width: 60vw;
+	height: 200px;
+`;
 const Heading = styled.h1`
-  margin-top:10px;
-  width:${getDeviceType() === "mobile" ? "35vw" :"61.8vw"};
-  height:${getDeviceType() === "mobile" ? "30vh" : "45vh"};
-  font-size:${getDeviceType() === "mobile" ? "30px" :"80px"};
-  font-family:time;
-  position:absolute;
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  background-color:rgba(0,0,0,0.25); 
-  color:#00203fff;
-  border-radius: 20px;
-`
+	margin: 10px auto;
+	width: ${getDeviceType() === "mobile" ? "80vw" : "80vw"};
+	height: ${getDeviceType() === "mobile" ? "200px" : "400px"};
+	font-size: ${getDeviceType() === "mobile" ? "30px" : "80px"};
+	font-family: time;
+	position: absolute;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: rgba(0, 0, 0, 0.25);
+	color: #00203fff;
+	border-radius: 20px;
+`;
 const Image = styled.img`
 	margin-top: 10px;
-  max-height: 45vh;
-  border-radius:20px;
-  margin-bottom:20px;
-`
+	border-radius: 20px;
+	width: 80vw;
+	height: ${getDeviceType() === "mobile" ? "200px" : "400px"};
+`;
