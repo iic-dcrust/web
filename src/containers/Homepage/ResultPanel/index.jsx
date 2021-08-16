64cbf49984e7bf6563888ef7350463ec40dcac47
Chaitanya31612 <guptachaitanya31@gmail.com>
2021-08-16 17:52:48 +0530

    result styled

--- src/containers/Homepage/ResultPanel/index.jsx
+++ src/containers/Homepage/ResultPanel/index.jsx
@@ -4,32 +4,30 @@ import styled from "styled-components";
 import { appColors } from "styles/colors";
 import resultData from "./resultsdata";
 
-import "./resultpanel.css";
-
 const ResultPanel = () => {
   console.log(resultData);
 
   return (
     <Container>
       <Head>Results of Idea Challenge</Head>
-      <div className="result-container">
+      <div>
         {resultData.map(({ name, leader, domains, members }, index) => (
           <>
-            <div key={index} className="result">
-              <h2 className="result-index">{index + 1}.</h2>
-              <h3 className="result-heading">{name}</h3>
-              <p className="result-lead">{leader}</p>
-              <div className="result-members">
+            <Result key={index}>
+              <Index>{index + 1}.</Index>
+              <Heading>{name}</Heading>
+              <TeamLeader>{leader}</TeamLeader>
+              <TeamMembers>
                 {members.map((member) => (
-                  <span className="result-member">{member}</span>
+                  <TeamMember>{member}</TeamMember>
                 ))}
-              </div>
-              <div className="result-domains">
+              </TeamMembers>
+              <div>
                 {domains.map((domain) => (
-                  <span className="result-domain">{domain} </span>
+                  <Domain>{domain} </Domain>
                 ))}
               </div>
-            </div>
+            </Result>
           </>
         ))}
       </div>
@@ -56,4 +54,50 @@ const Head = styled.h1`
   text-underline-offset: 10px;
   text-align: center;
   padding: 0 0;
+`;
+const Result = styled.div`
+  background-color: #f3f3f5;
+  margin-bottom: 2rem;
+  padding: 2.5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  transition: transform 0.3s ease;
+`;
+const Index = styled.h2`
+  font-size: 3em;
+  font-weight: 800;
+  z-index: 1;
+  opacity: 0.1;
+`;
+const Heading = styled.h3`
+  font-size: 1.4rem;
+  color: #333;
+  margin-bottom: 1rem;
+`;
+const TeamLeader = styled.p`
+  font-size: 1.1rem;
+  font-weight: 500;
+  color: #567;
+  margin-bottom: 1rem;
+`;
+const TeamMembers = styled.div`
+  margin-bottom: 1.3rem;
+`;
+const TeamMember = styled.span`
+  color: #666;
+  font-size: 1rem;
+
+  &:not(:last-child)::after {
+    content: ", ";
+  }
+`;
+const Domain = styled.span`
+  font-size: 0.9rem;
+  background-color: #bde1f1;
+  border-radius: 100px;
+  color: #0489c7;
+  padding: 2px 20px;
+  margin-right: 10px;
 `;
